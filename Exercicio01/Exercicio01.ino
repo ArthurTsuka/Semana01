54b5a141722cf6abe956239f94fa6c9e05dd5f8b
ViniciusKK <vinicius.kumagai@sou.inteli.edu.br>
2023-04-21 09:20:39 -0300

    .

---
+++ Exercicio01/Exercicio01.ino
@@ -0,0 +1,31 @@
+#include <SoftwareSerial.h>
+
+const int LED1 = 8;
+const int LED2 = 9;
+int pisca = 10;
+
+
+void setup() {
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  Serial.begin(9600);
+
+}
+void loop() {
+  // Pergunta no Serial Monitor o nome.
+  Serial.println("Qual o seu nome? ");
+  while(!Serial.available());
+  String nome = Serial.readString();
+  
+  // Pergunta qual produto o usuario quer.
+  Serial.println("Qual produto voce quer? ");
+  while(!Serial.available());
+  String produto = Serial.readString();
+  
+  // Pergunta o saldo do usario.
+  Serial.println("Quanto dinheiro possui? ");
+  while(!Serial.available());
+  String dinheiro = Serial.readString();
+  float valor = atof(dinheiro.c_str());
+
+
